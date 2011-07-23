eabebd7f6f103c44cef8993e00c8d492a6d06089
june29 <june29.jp@gmail.com>
2011-07-23 17:08:16 +0900

    first commit

---
+++ Gemfile
@@ -0,0 +1,20 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.1.0.rc4'
+
+gem 'sqlite3'
+
+gem 'sass-rails', "~> 3.1.0.rc"
+gem 'coffee-script'
+gem 'uglifier'
+
+gem 'jquery-rails'
+
+group :production do
+  gem 'pg'
+  gem 'therubyracer-heroku'
+end
+
+group :test do
+  gem 'turn', :require => false
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tweets::Application.config.secret_token = '4d47942d79b0449e948db41b09245bb320f88ea3c9b840d577b9caba9fe9db625f445faf62aa3232a8eefb7904655bad39c8f46f6a28c0106d63bc6092e689e0'
